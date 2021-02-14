2fbf4ffe123625641946f02e729f38fde7f30af8
Jonatan Shoshan <jonatan-bs@live.dk>
2021-02-14 15:07:49 +0100

    Added footer content

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,45 @@
 import styled from "styled-components";
+import Link from "next/link";
 
+const Section = styled.section`
+    background: #1a1919;
+    color: #fff;
+    padding: 50px;
+`
+
+const Logo = styled.img.attrs({ src:"/images/logo-light.svg" })`
+    max-width: 400px; 
+`
 
 const Footer = () => (
-    <>
-        <p>Footer</p>
-    </>
+    <Section>
+        <div>
+            <Logo/>
+            <p>
+                Søndermarksvej 114 stmf.<br/>
+                7000 Fredericia <br/><br/>
+
+                CVR.: 00000000<br/>
+            </p>
+            <Link href="#">
+                <a>kontakt@shoshandevelopment.dk</a>
+            </Link>
+        </div>
+        <div>
+            SERVICES
+            Hjemmeside
+            Webshop
+            Webapplikation
+            Support
+        </div>
+        <div>
+            SHOSHAN DEV. 
+            Om mig
+            Teknologier
+            Designprocess
+        </div>
+
+        
+    </Section>
 )
 export default Footer 
