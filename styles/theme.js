c2c74b644230c3760896cca9b2ac1dbbd30bad50
Jonatan Shoshan <jonatan-bs@live.dk>
2021-02-14 15:08:10 +0100

    Added extra colors to theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,8 +1,11 @@
 const theme = () => ({
   colors: {
     primary: '#bf2742',
-    primaryDark: '#7d0319'
-
+    primaryDark: '#7d0319',
+    dark: '#343434',
+    light: '#afadad',
+    light2: '#f3f3f3',
+    light3: '#fae9ec',
   },
   mediaQueries: {
     md: 'only screen and (min-width: 769px)'
