72efb23a7160db63f0ecec9f07528fb4bf9e73c2
Jonatan-bs <jonatan-bs@live.dk>
2021-04-13 19:28:19 +0200

    Removed Heading on services (On frontpage)

--- components/frontpage/Services.js
+++ components/frontpage/Services.js
@@ -3,12 +3,13 @@ import {AspectRatio, Container, Heading, Text, Image} from "./../partials"
 import { getStrapiMedia, fetchAPI } from "../../lib/api";
 
 const ServiceCard = styled.article`
-    width: 200px;
+    width: 33%;
 
 `
 const Price = styled.p`
     font-weight:bold;
     margin-top:10px;
+    text-align: center;
 `
 
 const ServiceGrid = styled.div`
@@ -21,47 +22,16 @@ const ServiceGrid = styled.div`
 const AspectRatioMod = styled(AspectRatio)`
     margin: 0 auto;
 `
-const HeadingImgWrap = styled.div`
-    display:flex;
-    align-items: center;
-    padding: 150px 0;
-    justify-content: center;
 
-`
-const HeadingWrap = styled.div`
-    // width: 70%;
-`
-const ImgWrap = styled.div`
-    // width: 30%;
-    max-width: 400px;
-    padding-right: 30px;
-    box-sizing: border-box;
-`
-
-// const Heading = styled.h3`
-//     font-size: 8rem;
-//     font-weight: bold;
-// `
 
 const Services = ({services}) => (
     <section>
-        <Container pb={3}>
-            <HeadingImgWrap>
-                <ImgWrap data-aos="fade-right">
-                    <Image src="/images/sewing-machine.png" alt="skræddersyget - illustration" />
-                </ImgWrap>
-                <Heading as="h3" size="xl"  data-aos="fade-left">
-                    Skræddersyede <br/> Webløsninger
-                </Heading>
-            </HeadingImgWrap>
+        <Container py={3}>
             <ServiceGrid>
                 {services && services.map((service, i) => (
                     <ServiceCard key={i} data-aos="fade-up"> 
-                        <AspectRatioMod src={getStrapiMedia(service.symbol)} pct="100" width="100%" maxWidth="100px" />
+                        {service.symbol && <AspectRatioMod src={getStrapiMedia(service.symbol)} pct="100" width="100%" maxWidth="100px" />}
                         <Heading center as="h4" py={.2} size="sm" caps>{service.title}</Heading>
-                        <Text size="sm">
-                            {service.elements.map( ({entry}, i)=> <span key={i}>{entry} <br/> </span>)}
-                        </Text>
                         <Price>{service.price}</Price>
                     </ServiceCard>
                 ))}
