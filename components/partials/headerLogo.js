41fb9d6aee569c04caba1197f2b72f5ee24f457c
Jonatan-bs <jonatan-bs@live.dk>
2021-04-14 03:30:21 +0200

    ...

--- components/partials/headerLogo.js
+++ components/partials/headerLogo.js
@@ -5,7 +5,7 @@ const LogoWrap = styled.div`
     width: 100%;
     text-align: center;
 `
-const Logo = styled.img.attrs({ src:"images/logo.svg", alt: "Shoshan Development" })`
+const Logo = styled.img.attrs({ src:"/images/logo.svg", alt: "Shoshan Development" })`
     width:100%;
     max-width:330px;
     margin: 50px;
