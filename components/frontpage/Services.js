50ad6ef6c9837e84311834f027e61e187c9cdc4d
Jonatan-bs <jonatan-bs@live.dk>
2021-04-25 21:47:25 +0200

    ...

--- components/frontpage/Services.js
+++ components/frontpage/Services.js
@@ -46,12 +46,12 @@ const Services = ({services}) => (
     <section>
         <ServiceGrid py={2}>
             {services && services.map((service, i) => (
-                <ServiceCardWrap  data-aos="fade-up">
+                <ServiceCardWrap  key={i} data-aos="fade-up">
                     <Link href={service?.link?.full_slug ? "/" + service.link.full_slug : "#"} color="dark">
                             <ServiceCard key={i}> 
                                     {service.symbol && <Image src={getStrapiMedia(service.symbol)} width="100px" height="100px" />}
                                     <Heading center as="h4" py={.2} size="md" caps>{service.title}</Heading>
-                                    {service.elements && <Text color="dark3" size="xs" mb="0" > {service.elements?.map( ({entry, bullet}, i) => entry + ((service.elements.length)>i+1? ", " : ""))}</Text>}
+                                    {service.elements && <Text color="dark3" size="xs" mb="0" > {service.elements?.map( ({entry, bullet}, i) =>(<span key={i}>{ entry + ((service.elements.length)>i+1? ", " : "")}</span>))}</Text>}
                                     <Price bold center size="sm">{service.price}</Price>
                                     {service.subtext && <Text italic color="dark3" size="xs" center>{service.subtext}</Text>}
                             </ServiceCard>
